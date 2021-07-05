1d397cfa31e5ad829f5ab911b56abcdb4628253f
Leandre <leajapo@gmail.com>
2021-07-05 11:28:16 +0200

    feat: added user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { model, Schema } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        fname: { type: String, required: true },
+        lname: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        role: { type: String, required: true, enum: ['supplier', 'customer'] },
+    },
+    { timestamps: true }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
