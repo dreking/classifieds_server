b1ba4b30f788d5405be44230fb448e9aae7ec5b3
Leandre <leajapo@gmail.com>
2021-07-05 11:47:20 +0200

    feat: registered route structure and versions

---
+++ routes/v1.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const authRoutes = require('./auth');
+const sellersRoutes = require('./seller');
+
+router.use('/auth', authRoutes);
+
+router.use('/sellers', sellersRoutes);
+
+module.exports = router;
